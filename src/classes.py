747d18ac5febe5b49acabff15e9c65b76596b284
Mutikova.Ya <mutikovaana@gmail.com>
2025-05-12 12:31:37 +0300

    14-2 выполнено

--- src/classes.py
+++ src/classes.py
@@ -2,18 +2,52 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
 
+    @property
+    def price(self):
+        """Геттер для получения цены"""
+        return self.__price
 
+    @price.setter
+    def price(self, new_price):
+        """Сеттер с проверкой: цена не может быть 0 или меньше"""
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
+    @classmethod
+    def new_product(cls, data):
+        """Класс-метод для создания продукта из словаря."""
+        return cls(data["name"], data["description"], data["price"], data["quantity"])
+
+
+# Класс Категория (Category)
 class Category:
     total_categories = 0
     total_products = 0
 
-    def __init__(self, name, description, products):
+    def __init__(self, name, description):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = []
 
         Category.total_categories += 1
-        Category.total_products += len(products)
+
+    def add_product(self, product):
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.total_products += 1
+        else:
+            print("Можно добавлять только товары (Product)")
+
+    @property
+    def products(self):
+        """Геттер для получения списка товаров в читаемом виде."""
+        result = []
+        for p in self.__products:
+            line = f"{p.name}, {p.price} руб. Остаток: {p.quantity} шт."
+            result.append(line)
+        return result
